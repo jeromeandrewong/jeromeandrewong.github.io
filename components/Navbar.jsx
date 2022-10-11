5aa84471955d62200b9e83e02d7ee1588adf0554
Jerome Wong <jeromewongjh@gmail.com>
2022-10-12 05:21:09 +0800

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="">
       <nav className="fixed z-10 text-center py-4 px-6 bg-white bg-opacity-50 backdrop-blur-lg  drop-shadow-lg w-full">
         <div className="flex flex-col items-center sm:flex-row sm:justify-center sm:gap-16">
-          <div class="mb-2 sm:mb-0 hover:text-blue-500 text-2xl font-semibold tracking-tight">
+          <div className="mb-2 sm:mb-0 hover:text-blue-500 text-2xl font-semibold tracking-tight">
             <Link href="#hero">Home</Link>
           </div>
           <div className="mb-2 sm:mb-0 hover:text-blue-500 text-2xl font-semibold tracking-tight">
